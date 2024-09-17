af401f1f11f454ed6b2a4d63fec24fd2dd67bb46
shanejeremich <shane.jeremich@gmail.com>
2024-09-17 05:48:18 -0600

    chore(db): update DB config and .env with remote MongoDB connection - Updated database configuration and .env file with connection string for remote MongoDB instance.

--- api/app/db/config.js
+++ api/app/db/config.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  console.log(process.env.MONGODB_URI);
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
 
+const connectDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI);
+    const db = await mongoose.connect(MONGODB_URI);
     console.log(`MongoDB Connected: \t\t ${db.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
